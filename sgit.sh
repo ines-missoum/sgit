05268f0a476d8205836e7f6d5317db372808f574
Ines Missoum <inesmiss@live.fr>
2019-10-05 20:04:32 +0200

    UPDATE archive path available everywhere

--- sgit.sh
+++ sgit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-scala 'target/scala-2.13/sgit-assembly-0.1.jar'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+scala "$DIR/target/scala-2.13/sgit-assembly-0.1.jar"
