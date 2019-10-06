ccf12769572aa7e701608179b584c1cd8ef7c40b
Ines Missoum <inesmiss@live.fr>
2019-10-06 12:25:47 +0200

    ADD location path to display

--- src/main/scala/fr/missoum/utils/ConsolePrinter.scala
+++ src/main/scala/fr/missoum/utils/ConsolePrinter.scala
@@ -1,12 +1,13 @@
 package fr.missoum.utils
+import java.lang.System._
 
 object ConsolePrinter {
 
   def sgitFolderAlreadyExists() ={
-    println ("Reinitialized existing Git repository in /????/.sgit/")
+    println ("Reinitialized existing Git repository in "+System.getProperty("user.dir")+"/.sgit/")
   }
 
   def sgitFolderCreated() ={
-    println ("Initialized empty Git repository in /????/.sgit/")
+    println ("Initialized empty Git repository in "+System.getProperty("user.dir")+"/.sgit/")
   }
 }
