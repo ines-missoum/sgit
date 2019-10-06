b642495e606c3240909f10a2211134fadf73a104
Ines Missoum <inesmiss@live.fr>
2019-10-06 12:16:17 +0200

    ADD arguments when run

--- sgit.sh
+++ sgit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-scala "$DIR/target/scala-2.13/sgit-assembly-0.1.jar"
+scala "$DIR/target/scala-2.13/sgit-assembly-0.1.jar" $@
