8d959a9fc56e4a72458b3e95d535d01c94a41b3b
Ines Missoum <inesmiss@live.fr>
2019-10-14 12:49:55 +0200

    UPDATE scaladoc review

--- src/main/scala/fr/missoum/commands/SgitStatusImpl.scala
+++ src/main/scala/fr/missoum/commands/SgitStatusImpl.scala
@@ -4,6 +4,13 @@ import fr.missoum.logic.EntryTree
 
 object SgitStatusImpl extends SgitStatus {
 
+  /**
+   * By comparing the index and commits, it returns all the blobs to be committed divided in three categories : new files, modified files and deleted files
+   *
+   * @param index blobs of the index
+   * @param lastCommit blobs that have been committed in the previous commit in their last version
+   * @return three lists (each one for a category), in the order : new files, modified files and deleted files
+   */
   def getChangesToBeCommitted(index: Array[EntryTree], lastCommit: Array[EntryTree]): (Array[String], Array[String], Array[String]) = {
     val news = inFirstListButNotInSecond(index, lastCommit).map(_.path)
     val modified = getModifiedElements(index, lastCommit).map(_.path)
@@ -11,12 +18,24 @@ object SgitStatusImpl extends SgitStatus {
     (news, modified, deleted)
   }
 
+  /**
+   * By comparing the index and workspace, it returns all the blobs not stage for commit divided in two categories : modified files and deleted files
+   * @param index blobs of the index
+   * @param workspace blobs of the workspace
+   * @return two lists (each one for a category), in the order : modified files and deleted files
+   */
   def getChangesNotStagedForCommit(index: Array[EntryTree], workspace: Array[EntryTree]): (Array[String], Array[String]) = {
     val modified = getModifiedElements(index, workspace).map(_.path)
     val deleted = inFirstListButNotInSecond(index, workspace).map(_.path)
     (modified, deleted)
   }
 
+  /**
+   * By comparing the index and workspace, it returns all the blobs not tracked
+   * @param workspace blobs of the workspace
+   * @param index blobs of the index
+   * @return the list of blobs untracked
+   */
   def getUntrackedFiles(workspace: Array[EntryTree], index: Array[EntryTree]): Array[String] = {
     inFirstListButNotInSecond(workspace, index).map(_.path)
   }
